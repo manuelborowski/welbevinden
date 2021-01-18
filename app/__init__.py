42fccaa4f7d8d6f3503b4283450a6bd75a442a1a
manuel borowski <emmanuel.borowski@gmail.com>
2021-01-18 12:26:51 +0100

    V0.11 : added supervisor access

--- app/__init__.py
+++ app/__init__.py
@@ -24,12 +24,13 @@ flask_app = Flask(__name__, instance_relative_config=True, template_folder='pres
 # V0.8 : update in teams-meetings : multiple meetings possible
 # V0.9 : aesthetic update
 # V0.10 : bugfixed edit registration
+# V0.11 : added supervisor access
 
 
 
 @flask_app.context_processor
 def inject_version():
-    return dict(version='V0.10')
+    return dict(version='V0.11')
 
 #enable logging
 LOG_HANDLE = 'SIAB'

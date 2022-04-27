320c22c84d845789e6614922df68b56dfe6a2442
Manuel Borowski <manuel.borowski@campussintursula.be>
2022-04-27 16:36:20 +0200

    V0.91: layout update

--- app/__init__.py
+++ app/__init__.py
@@ -121,6 +121,7 @@ flask_app.config.from_pyfile('config.py')
 # V0.88; update navbar, icon and backgroundpicture
 # V0.89: add student: insert current datetime.  Delete students
 # V0.90: update requirements.txt
+# V0.91: layout update
 
 
 #TODO: add sequence numbers when on the waiting list.  Add them on the confirmation document?
@@ -139,7 +140,7 @@ flask_app.config.from_pyfile('config.py')
 
 @flask_app.context_processor
 def inject_defaults():
-    return dict(version='@ 2022 MB. V0.90', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
+    return dict(version='@ 2022 MB. V0.91', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
 
 
 #  enable logging

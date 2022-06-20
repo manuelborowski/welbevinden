59a8f4c5753e1c5523b878871f7068c9b59220a5
Manuel Borowski <manuel.borowski@campussintursula.be>
2022-06-20 08:18:37 +0200

    0.9: update python-package, use configurable IP address

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import flask_app, socketio
 
 if __name__ == '__main__':
-    socketio.run(flask_app, port=flask_app.config['FLASK_PORT'], host='127.0.0.1')
+    socketio.run(flask_app, port=flask_app.config['FLASK_PORT'], host=flask_app.config['FLASK_IP'])
 
 #if started from the commandline:
 #venv/bin/uwsgi --http :5000 --gevent 1000 --http-websockets --master  -w run:flask_app --logto log-file
